8d3f19ef598fe27c3f1eb4b66fe3d6bbdfd402c9
Stevenijames <ijamesproductions1@gmail.com>
2023-01-30 15:36:30 -0700

    added media queries and updated styling to Footer.js, Global inblock-line in App.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,84 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer>
+      <section className="footer-content">
+        <h3 className="email">
+          <span className="info-first">
+            <HiOutlineMail className="icon inline-block" />
+            Email:{' '}
+          </span>
+          <span className="info-second">spijames2021@gmail.com</span>
+        </h3>
+        <h3 className="phone">
+          <span className="info-first">
+            <SlCallOut className="icon inline-block" />
+            Phone:{' '}
+          </span>
+          <span className="info-second">(720) 556-8285</span>
+        </h3>
+        <a href={resumeFile}>
+          <button className="resume-button">
+            Resume
+            <FiDownload className="icon inline-block" />
+          </button>
+        </a>
+        <div className="icon-container">
+          <a href="https://www.facebook.com/spijames1" target="_blank" rel="noreferrer">
+            <button className="social-btn">
+              <GrFacebook />
+            </button>
+          </a>
+          <a href="https://github.com/Stevenijames" target="_blank" rel="noreferrer">
+            <button className="social-btn">
+              <GoMarkGithub />
+            </button>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/paul-ijames/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="social-btn">
+              <BsLinkedin />
+            </button>
+          </a>
+        </div>
+      </section>
+
+      <section className="media">
+        <h5>Media</h5>
+        <ul>
+          <li>
+            <a href="https://youtu.be/mUA9CXEqvZM" target="_blank" rel="noreferrer">
+              YouTube
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://open.spotify.com/artist/6Tn5Dy7vmNzUYoRHsVZYfI?si=UNPNhBCSRoKAZqk4DUjBew"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Spotify
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.thebash.com/event-dj/dj-paul-ijames-ijames-productions"
+              target="_blank"
+              rel="noreferrer"
+            >
+              DJ
+            </a>
+          </li>
+          <li>
+            <a href="https://youtu.be/YdWhO9lK1J8" target="_blank" rel="noreferrer">
+              More Media
+            </a>
+          </li>
+        </ul>
+      </section>
       <section className="footer-contributions">
         <h5>Contributions & Affiliations</h5>
         <ul>
@@ -43,77 +120,6 @@ const Footer = () => {
               rel="noreferrer"
             >
               Chromosome 9pminus Network
-            </a>
-          </li>
-        </ul>
-      </section>
-      <section className="footer-content">
-        <h3 className="email">
-          <HiOutlineMail className="icon" />
-          Email: spijames2021@gmail.com
-        </h3>
-        <h3 className="phone">
-          <SlCallOut className="icon" />
-          Phone: (720) 556-8285
-        </h3>
-        <a className="footer-button" href={resumeFile}>
-          <button>
-            Resume
-            <FiDownload className="icon" />
-          </button>
-        </a>
-      </section>
-      <div className="icon-btn">
-        <a href="https://www.facebook.com/spijames1" target="_blank" rel="noreferrer">
-          <button className="footer-btn">
-            <GrFacebook />
-          </button>
-        </a>
-        <a href="https://github.com/Stevenijames" target="_blank" rel="noreferrer">
-          <button className="footer-btn">
-            <GoMarkGithub />
-          </button>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/paul-ijames/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button className="footer-btn">
-            <BsLinkedin />
-          </button>
-        </a>
-      </div>
-
-      <section className="media">
-        <h5>Media</h5>
-        <ul>
-          <li>
-            <a href="https://youtu.be/mUA9CXEqvZM" target="_blank" rel="noreferrer">
-              YouTube
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://open.spotify.com/artist/6Tn5Dy7vmNzUYoRHsVZYfI?si=UNPNhBCSRoKAZqk4DUjBew"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Spotify
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.thebash.com/event-dj/dj-paul-ijames-ijames-productions"
-              target="_blank"
-              rel="noreferrer"
-            >
-              DJ
-            </a>
-          </li>
-          <li>
-            <a href="https://youtu.be/YdWhO9lK1J8" target="_blank" rel="noreferrer">
-              More Media
             </a>
           </li>
         </ul>
