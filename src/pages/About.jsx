f23d0b91eec019585e58e74e5eee9970792c99c1
Stevenijames <ijamesproductions1@gmail.com>
2023-01-30 12:26:19 -0700

    fixed picture import in About.js

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,9 @@ import './About.css'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
+import CodingL from '../assets/img/codinglanguagesresized.jpg'
+import FamilyPhoto from '../assets/img/beachphoto1.jpg'
+import PhotoCollage from '../assets/img/paul-collage.jpg'
 
 const About = () => {
   return (
@@ -11,17 +14,17 @@ const About = () => {
       <main>
         <section className="about-cards">
           <Card
-            img="/src/assets/img/codinglanguagesresized.jpg"
+            img={CodingL}
             title="Web Developer"
             description="I am a Full Stack Web Developer that has a passion for Frontend design. My langagues include HTML, CSS, JavaScript, Node.js, within modern frameworks like React.js and Vue.js. I have a passion for creating web apps."
           />
           <Card
-            img="/src/assets/img/beachphoto1.jpg"
+            img={FamilyPhoto}
             title="Family - Travel"
             description="I love to make memories with my wife and two daughters. Last year I traveled 26,965 miles within the United States, visting 78 cities and 447 places, 236 which were new. In a nutshell, I work to have the ability to travel."
           />
           <Card
-            img="/src/assets/img/paul-collage.jpg"
+            img={PhotoCollage}
             title="At The Core"
             description="Faith, Family and Friends are key concepts that I believe in. I have worked as a musician for the greater part of my life and web developer since Fall 2020. I attribute my success to the ones who believed in me."
           />
